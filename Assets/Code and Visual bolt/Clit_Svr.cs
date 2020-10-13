a4b553132a940cb558be44b95ba65367d7c44dba
Zyumane <64087119+Zyumane@users.noreply.github.com>
2020-10-13 10:35:09 -0500

    Update Clit_Svr.cs
    api need fix

--- Assets/Code and Visual bolt/Clit_Svr.cs
+++ Assets/Code and Visual bolt/Clit_Svr.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Clit_Svr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public bool isAtStartup = true;
+    public NetworkClient myClient;
     void Update()
     {
-        
+        if (isAtStartup)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                SetupServer();
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                SetupClient();
+            }
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                SetupServer();
+                SetupLocalClient();
+            }
+        }
+    }
+    void OnGUI()
+    {
+        if (isAtStartup)
+        {
+            GUI.Label(new Rect(2, 10, 150, 100), "Press S for server");
+            GUI.Label(new Rect(2, 30, 150, 100), "Press B for both");
+            GUI.Label(new Rect(2, 50, 150, 100), "Press C for client");
+        }
     }
 }
